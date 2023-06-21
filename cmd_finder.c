a025fd13fc1371499d6ecfd2021477f0bdb0749e
Orino1 <orinomohamed@gmail.com>
2023-06-21 04:42:44 +0100

    Adding cmd_finder.c File

---
+++ cmd_finder.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+//to do: handle nigative numbers
+int cmd_finder(char *token, int cmd_num, stack_t **top_element, unsigned int line_number)
+{
+    int i;
+
+    for (i = 0; i < cmd_num; i++)
+    {
+        if (strcmp(instruction_cmd[i].opcode, token) == 0)
+        {
+            instruction_cmd[i].f(top_element, line_number);
+            return (0);
+        }
+    }
+    return (1);
+}
+
+int line_checker(char *buffer)
+{
+    int i;
+
+    if (buffer[0] == '#')
+    {
+        return (1);
+    }
+    if (strcmp("\n", buffer) == 0)
+    {
+        return (1);
+    }
+    for (i = 0; buffer[i] != '\n'; i++)
+    {
+        if (buffer[i] != ' ')
+        {
+            return (0);
+        }
+    }
+    return (1);
+}
