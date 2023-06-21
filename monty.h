e612c1637d527130040406828dc032e8390ced73
Orino1 <orinomohamed@gmail.com>
2023-06-21 04:43:07 +0100

    Adding monty.h File

---
+++ monty.h
@@ -0,0 +1,40 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define BUFFER 1024
+typedef struct stack_s
+{
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
+} stack_t;
+typedef struct instruction_s
+{
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern instruction_t instruction_cmd[];
+
+
+int line_checker(char *buffer);
+
+stack_t *add_element_at_top(stack_t **top_element, int n);
+void pint(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+int cmd_finder(char *token, int cmd_num, stack_t **top_element, unsigned int line_number);
+
+#endif
