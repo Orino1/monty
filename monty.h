13efa38c574d900fca41afd6dcd8d59820a5960f
Orino1 <orinomohamed@gmail.com>
2023-06-21 20:14:51 +0100

    Adding monty.h File

--- monty.h
+++ monty.h
@@ -14,12 +14,16 @@ typedef struct instruction_s
 		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+extern stack_t *stack_head;
+extern char buffer_line[1024];
 extern instruction_t instruction_cmd[];
 
 
 int line_checker(char *buffer);
+void free_dlistint(stack_t *head);
 
 stack_t *add_element_at_top(stack_t **top_element, int n);
+
 void pint(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
@@ -35,6 +39,7 @@ void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
+
 int cmd_finder(char *token, int cmd_num, stack_t **top_element, unsigned int line_number);
 
 #endif
